e100ecfd9fb3a9edec580dbada987e50fc8f5cfe
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2022-09-25 21:27:11 +0900

    色々変更

--- src/ari/ari.ts
+++ src/ari/ari.ts
@@ -2,8 +2,15 @@ import p5 from "p5";
 
 import "./../style.css";
 // これを参考に作りたい
+
 new p5((p5Instance) => {
   const p = p5Instance as unknown as p5;
+  const WORLD_SIZE: { height: number; width: number } = {
+    height: 100,
+    width: 100,
+  };
+  let world = Array(WORLD_SIZE.height).fill(Array(WORLD_SIZE.width).fill(0));
+  console.log(JSON.stringify(world));
   p.setup = () => {
     p.createCanvas(500, 500);
     p.background(220);
