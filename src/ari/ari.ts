4bd9ba75b28a5f931bf77ca4bd2f1afd2f670a0b
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2022-09-27 12:52:54 +0900

    黒と白をいい感じに分けるようにした

--- src/ari/ari.ts
+++ src/ari/ari.ts
@@ -4,19 +4,25 @@ import "./ari.css";
 // これを参考に作りたい
 console.log("this is ari");
 const SquereColor = (state: number): string => {
-  return state === 1 ? "#22ff22" : "#00f9f9";
+  return state === 1 ? "#000000" : "#ffffff";
 };
+const generate2DArray2 = (m: number, n: number, val = 0) => {
+  return [...Array(m)].map((_) => Array(n).fill(val));
+};
+
 new p5((p5Instance) => {
   const p = p5Instance as unknown as p5;
   // ワールドの四角の数
   const WORLD_SIZE: { height: number; width: number } = {
-    height: 50,
-    width: 50,
+    height: 40,
+    width: 40,
   };
   const SquereSize: number = 20;
 
-  let world: number[][] = Array(WORLD_SIZE.height).fill(
-    Array(WORLD_SIZE.width).fill(0)
+  let world: number[][] = generate2DArray2(
+    WORLD_SIZE.width,
+    WORLD_SIZE.height,
+    0
   );
   console.log({ world: world[2][1] });
   console.log(JSON.stringify(world, null, 2));
@@ -34,7 +40,7 @@ new p5((p5Instance) => {
       value.map((state, w) => {
         p.fill(p.color(SquereColor(state)));
         p.square(h * SquereSize, w * SquereSize, SquereSize);
-        p.fill(p.color(SquereColor(0)));
+        // p.fill(p.color(SquereColor(0)));
       });
     });
   };
